df2ce4cb656484dc9ad49545d6d53ed0940d1d10
Diego <dsanthony997@gmail.com>
2018-01-16 16:17:40 -0500

    Controls and box color

--- ColorSwitcher/ColorSwitcher/MainPage.xaml.cs
+++ ColorSwitcher/ColorSwitcher/MainPage.xaml.cs
@@ -12,6 +12,25 @@ namespace ColorSwitcher
 		public MainPage()
 		{
 			InitializeComponent();
-		}
-	}
+            sldRojo.ValueChanged += sldChanged;
+            sldVerde.ValueChanged += sldChanged;
+            sldAzul.ValueChanged += sldChanged;
+        }
+
+        private void sldChanged(object sender, ValueChangedEventArgs e)
+        {
+            Color color = new Color(sldRojo.Value,sldVerde.Value,sldAzul.Value);
+            BoxColor.Color = color;
+            codHexa.Text = ColorToHexadecimal(color);
+        }
+
+        private string ColorToHexadecimal(Color color)
+        {
+            var rojo = (int)(color.R *255);
+            var verde = (int)(color.G * 255);
+            var azul = (int)(color.B * 255);
+            var alpha = (int)(color.A * 255);
+            return $"#{rojo:X2}{verde:X2}{azul:X2}{alpha:X2}";
+        }
+    }
 }
